082015cfda2e03c831703c343ba25d47c7bdbb4f
lsw <hkbvc1222@gmail.com>
2024-10-30 03:05:17 +0900

    chore: 린트 , alias 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@entity": path.resolve(__dirname, "src/entity"),
+      "@app": path.resolve(__dirname, "src/app"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
